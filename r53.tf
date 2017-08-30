0ca95aaa501a1d3bab6e669d7e38a82c53f25fe8
Philip Dubois <hello@philipdubois.be>
2017-08-30 17:30:12 +0200

    Add zonde_id check for DKIM records

--- r53.tf
+++ r53.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "domain_amazonses_verification_record" {
 }
 
 resource "aws_route53_record" "domain_amazonses_dkim_record" {
-  count   = "${length(var.dkim_records)}"
+  count   = "${var.zone_id != "" ? length(var.dkim_records) : 0}"
   zone_id = "${var.zone_id}"
   name    = "${element(keys(var.dkim_records), count.index)}"
   type    = "CNAME"
