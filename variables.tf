49ea43e457b264bad6e0ce0431c9e980254c804e
Jakob <info@jakobniggel.de>
2018-04-09 13:33:10 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,6 +14,6 @@ variable "dkim_records" {
 }
 
 variable "ses_records" {
-  description = "Additionals entries which are add to the _amazonses record"
+  description = "Additionals entries which are added to the _amazonses record"
   default = []
 }
